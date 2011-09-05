9f5d268f5b35a08da3e9010913e94987a5ba4369
bryanduxbury <bryan@rapleaf.com>
2011-09-04 22:26:15 -0700

    started working on variable anchor points based on length... something definitely not quite right.

--- boxer.rb
+++ boxer.rb
@@ -1,32 +1,48 @@
 def draw_face(x, y, w, h, material_thickness)
-  print "<path d='"
-
-  print "M#{x} #{y}"
-
-  print "L#{x} #{y + h}"
-  print "L#{x + w} #{y + h}"
-  print "L#{x + w} #{y}"
-  puts "Z' fill='none' stroke='black' stroke-width='1' />"
+  puts <<-EOF 
+  <g transform='translate(#{x} #{y})'>
+    <path d="
+      M0 0
+      h#{w}
+      v#{h}
+      h-#{w}
+      v-#{h}
+      "
+      class="cutline"
+    />
+    <rect class="cutline" x="#{w/2 - 5}" y="#{material_thickness}" width="10" height="#{material_thickness}" />
+    <rect class="cutline" x="#{w/2 - 5}" y="#{h-2*material_thickness}" width="10" height="#{material_thickness}" />
+    <rect class="cutline" x="#{material_thickness}" y="#{h/2-5}" width="#{material_thickness}" height="10" />
+    <rect class="cutline" x="#{w-2*material_thickness}" y="#{h/2-5}" width="#{material_thickness}" height="10" />
+  </g>
+  EOF
 end
 
 def draw_side(x, y, w, h, material_thickness, rotate = false, flip = false, screw_width = 3.0)
+  num_anchors = (w / 100.0).ceil
+  dist_between_anchors = w / (num_anchors + 1)
+  anchors = ""
+  x = 0
+  0.upto(num_anchors) do |n|
+    anchors << "h#{dist_between_anchors/2-5}v#{material_thickness}h10v-#{material_thickness}"
+    [x+dist_between_anchors/2-5,x+dist_between_anchors/2+5]
+    x+=dist_between_anchors
+  end
+  anchors << "h#{dist_between_anchors/2-5}"
+
   puts <<-EOF
   <g transform='translate(#{x} #{y}) #{rotate ? "translate(#{h}, 0) rotate(90)" : "" } rotate(#{flip ? 180 : 0}, #{w/2}, #{h/2})'>
     <path d='
       M0 0
-      L0 #{h - material_thickness}
-      L#{w / 2 - 5} #{h - material_thickness}
-      L#{w / 2 - 5} #{h}
-      L#{w / 2 + 5} #{h}
-      L#{w / 2 + 5} #{h - material_thickness}
-      L#{w} #{h - material_thickness}
-      L#{w} 0
-      Z'
-      fill='none' stroke='black' stroke-width='1'
+      v#{h - material_thickness}
+      #{anchors}
+      v#{h - material_thickness}
+      h#{w}'
+      class="cutline"
     />
-    <rect x="#{w/2 - 5 - 10}" y="#{material_thickness}" width="10" height="#{material_thickness}" fill="none" stroke-width="1" stroke="black" />
-    <rect x="#{w/2 + 5}" y="#{material_thickness}" width="10" height="#{material_thickness}" fill="none" stroke-width="1" stroke="black" />
-    <circle cx="#{w/2}" cy="#{material_thickness + screw_width/2}" r="#{screw_width / 2}" fill="none" stroke-width="1" stroke="black" />
+    <rect class="cutline" x="#{w/2 - 5 - 10}" y="#{material_thickness}" width="10" height="#{material_thickness}" />
+    <rect class="cutline" x="#{w/2 + 5}" y="#{material_thickness}" width="10" height="#{material_thickness}" />
+    <circle class="cutline" cx="#{w/2}" cy="#{material_thickness + screw_width/2}" r="#{screw_width / 2}" />
   </g>
   EOF
 end
@@ -102,18 +118,9 @@ def draw_bottom(x, y, w, h, material_thickness, screw_width, screw_length)
       h#{material_thickness}
       v-#{h/2 - 15 - material_thickness}
     "
-    fill='none' stroke='black' stroke-width='1' />
+    class="cutline" />
   </g>
   EOF
-  # 
-  # print "<path d='"
-  # 
-  # print "M#{x} #{y}"
-  # 
-  # print "L#{x} #{y + h}"
-  # print "L#{x + w} #{y + h}"
-  # print "L#{x + w} #{y}"
-  # puts "Z' fill='none' stroke='black' stroke-width='1' />"
 end
 
 w, h, d = ARGV.shift.to_f, ARGV.shift.to_f, ARGV.shift.to_f
@@ -123,6 +130,43 @@ kerf = ARGV.shift.to_f
 
 SPACING = 5
 
+faceplate_width = w + material_thickness * 4
+faceplate_height = h + material_thickness * 4
+
+# top side
+side_width = w + material_thickness * 2
+side_height = d + material_thickness * 3
+
+top_side_origin_x = SPACING + faceplate_width / 2 - side_width / 2
+top_side_origin_y = SPACING
+
+# face
+faceplate_origin_x = SPACING
+faceplate_origin_y = top_side_origin_y + side_height + SPACING
+
+# bottom side
+bottom_side_origin_x = top_side_origin_x
+bottom_side_origin_y = faceplate_origin_y + faceplate_height + SPACING
+
+
+# right side
+right_side_width = h + material_thickness * 2
+right_side_height = d + material_thickness * 3
+
+right_side_origin_x = faceplate_origin_x + faceplate_width + SPACING
+right_side_origin_y = faceplate_origin_y + faceplate_height / 2 - right_side_width / 2
+
+# back
+back_width = w + material_thickness * 2
+back_height = h + material_thickness * 2
+
+back_origin_x = right_side_origin_x + side_height + SPACING
+back_origin_y = faceplate_origin_y + faceplate_height / 2 - right_side_width / 2
+
+# left side
+left_side_x = back_origin_x + back_width + SPACING
+left_side_y = right_side_origin_y
+
 # header
 
 puts <<-EOF
@@ -131,11 +175,7 @@ puts <<-EOF
 <head>
 <title>svg test</title>
 <style>
-stop.begin { stop-color:yellow; }
-stop.end { stop-color:green; }
-body.invalid stop.end { stop-color:red; }
-#err { display:none; }
-body.invalid #err { display:inline; }
+  .cutline {fill:none; stroke:black; stroke-width: .25}
 </style>
 <script>
 function signalError() {
@@ -145,57 +185,15 @@ document.getElementById('body').setAttribute("class", "invalid");
 </head>
 <body id="body" style="position:absolute; z-index:0; border:1px solid black; left:0%; top:0%; width:100%; height:100%;">
   <svg xmlns="http://www.w3.org/2000/svg" version="1.1"
-    viewBox="0 0 300 300" preserveAspectRatio="xMinYMin"
+    viewBox="0 0 #{SPACING * 5 + faceplate_width + 2*right_side_width + back_width} #{SPACING * 4 + 2 * side_height + faceplate_height}" preserveAspectRatio="xMinYMin"
     style="width:100%; height:100%; position:absolute; top:0; left:0; z-index:-1;">
 EOF
 
-# top side
-
-top_side_origin_x = SPACING
-top_side_origin_y = SPACING
-side_width = w + material_thickness * 2
-side_height = d + material_thickness * 3
-
 draw_side(top_side_origin_x, top_side_origin_y, side_width, side_height, material_thickness)
-
-# face
-
-faceplate_origin_x = SPACING
-faceplate_origin_y = top_side_origin_y + side_height + SPACING
-faceplate_width = w + material_thickness * 4
-faceplate_height = h + material_thickness * 4
 draw_face(faceplate_origin_x, faceplate_origin_y, faceplate_width, faceplate_height, material_thickness)
-
-# bottom side
-
-bottom_side_origin_x = SPACING
-bottom_side_origin_y = faceplate_origin_y + faceplate_height + SPACING
-
 draw_side(bottom_side_origin_x, bottom_side_origin_y, side_width, side_height, material_thickness, false, true)
-
-# right side
-
-right_side_origin_x = faceplate_origin_x + faceplate_width + SPACING
-right_side_origin_y = faceplate_origin_y
-right_side_width = h + material_thickness * 2
-right_side_height = d + material_thickness * 3
-
 draw_side(right_side_origin_x, right_side_origin_y, right_side_width, right_side_height, material_thickness, true, false)
-
-# back
-
-back_origin_x = right_side_origin_x + side_height + SPACING
-back_origin_y = faceplate_origin_y
-back_width = w + material_thickness * 2
-back_height = h + material_thickness * 2
-
 draw_bottom(back_origin_x, back_origin_y, back_width, back_height, material_thickness, screw_width, screw_length)
-
-# left side
-
-left_side_x = back_origin_x + back_width + SPACING
-left_side_y = faceplate_origin_y
-
 draw_side(left_side_x, left_side_y, right_side_width, right_side_height, material_thickness, true, true)
 
 puts <<-EOF
